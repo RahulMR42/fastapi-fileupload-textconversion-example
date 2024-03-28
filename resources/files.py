bed6675b47a9833a2b7fdd2b96a76631c059f67c
Rahul MR <test@example.com>
2024-03-28 20:08:20 +0530

    dir

--- resources/files.py
+++ resources/files.py
@@ -1,9 +1,12 @@
 import textract
+import os
 
 
 class Files:
-    def __init__(self):
-        self.input_path = 'inputs'
+    def __init__(self,tmp_input_path):
+        self.input_path = tmp_input_path
+        if not os.path.exists(self.input_path):
+            os.makedirs(self.input_path)
 
     def convert_to_text(self, file_name):
         try:
